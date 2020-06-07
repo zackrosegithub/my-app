fd8cd4c193e23847f6c1376d006b0a5786d145fc
Zack Rose <zackrose21@gmail.com>
2020-06-07 16:41:57 +0100

    done

---
+++ src/helpers.js
@@ -0,0 +1,21 @@
+function choice(items) {
+    let idx = Math.floor(Math.random()*items.length)
+    return items[idx];
+  }
+
+  export { choice } ;
+
+// function choice(items) {
+// 	let idx = Math.floor(Math.random() * items.length);
+// 	return items[idx];
+// }
+
+// function remove(items, item) {
+// 	for (let i = 0; i < items.length; i++) {
+// 		if (items[i] === item) {
+// 			return [ ...items.slice(0, i), ...items.slice(i + 1) ];
+// 		}
+// 	}
+// }
+
+// export { choice, remove };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import foods from './foods';
+// import { choice, remove } from './helpers';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { fruits } from './foods.js'
+import { choice } from './helpers.js'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let fruit = choice(fruits);
+
+console.log(`I'd like one ${fruit} please`)
+console.log(`Here you go: ${fruit}`)
+console.log(`Delicious! May I have another?`);
+
+
+
+// // Randomly draw a fruit from the array
+// let fruit = choice(foods);
+// // Log the message “I’d like one RANDOMFRUIT, please.”
+// console.log(`I’d like one ${fruit}, please.`);
+// // Log the message “Here you go: RANDOMFRUIT”
+// console.log(`Here you go: ${fruit}`);
+// // Log the message “Delicious! May I have another?”
+// console.log('Delicious! May I have another?');
+// // Remove the fruit from the array of fruits
+// let remaining = remove(foods, fruit);
+// // Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+// console.log(`I’m sorry, we’re all out. We have ${remaining.length} other foods left.`);
